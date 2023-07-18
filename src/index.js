c267826f987e095995c814dd0861c8bbe0abf925
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-07-18 17:31:37 -0500

    step 4

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+
+
 document.addEventListener('DOMContentLoaded', ()=>{
     fetch('http://localhost:3000/pups')
     .then(resp=>resp.json())
     .then(data=>data.forEach(dog=>printDog(dog)))
+    const filterButton = document.getElementById('good-dog-filter')
+    
+    filterButton.addEventListener('click', e=>{
+        e.preventDefault()
+        filterDogs()
+    })
 })
 
 function printDog(dogInfo){
     const dogContainer = document.getElementById('dog-bar')
     const dogName = document.createElement('span')
-
     dogName.innerText = dogInfo.name
     dogContainer.appendChild(dogName)
     dogName.addEventListener('click', e=>dogCard(dogInfo))
@@ -36,10 +43,34 @@ function dogCard(dogInfo){
 }
 
 function goodOrBad(dog){
-    console.log(dog.isGoodDog)
     if(dog.isGoodDog === true){
         return 'Good Dog!'
     } else {
         return "Bad Dog!"
+    }
+}
+
+function filterDogs(){
+    const filterButton = document.getElementById('good-dog-filter')
+    const dogContainer = document.getElementById('dog-bar')
+    const dogInfoContainer = document.getElementById('dog-info')
+    dogInfoContainer.innerText = ''
+
+    if(filterButton.innerText === 'Filter good dogs: OFF'){
+        filterButton.innerText = 'Filter good dogs: ON'
+        dogContainer.innerText = ''
+        fetch('http://localhost:3000/pups')
+        .then(resp=>resp.json())
+        .then(data=>data.forEach(dog=>{
+            if(dog.isGoodDog === true){
+                printDog(dog)
+            }
+        }))
+    } else {
+        filterButton.innerText = 'Filter good dogs: OFF'
+        dogContainer.innerText = ''
+        fetch('http://localhost:3000/pups')
+        .then(resp=>resp.json())
+        .then(data=>data.forEach(dog=>printDog(dog)))
     }
 }
